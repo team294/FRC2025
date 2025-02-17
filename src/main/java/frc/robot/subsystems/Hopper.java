15b8aa66ea40cb86d6d6639758c8d0f6e3461f04
Jenna Tripoli <jennamt1282@gmail.com>
2025-02-16 17:21:37 -0800

    Indentation and comments

--- src/main/java/frc/robot/subsystems/Hopper.java
+++ src/main/java/frc/robot/subsystems/Hopper.java
@@ -22,37 +22,30 @@ import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
 public class Hopper extends SubsystemBase {
   private final FileLog log;
   private final int logRotationKey;
-  private boolean fastLogging = false; // true is enabled to run every cycle; false follows normal logging cycles
-  private String subsystemName; // subsystem name for use in file logging and Shuffleboard
+  private boolean fastLogging = false;    // true = enabled to run every cycle, false = follow normal logging cycles
+  private String subsystemName;           // Subsystem name for use in file logging and dashboard
 
-  // Create Kraken variables for hopper motor
-  private final StatusSignal<Voltage> hopperSupplyVoltage; // Incoming bus voltage to motor conthopper, in volts
-  private final StatusSignal<Temperature> hopperTemp; // Motor temperature, in degC
-  private final StatusSignal<Double> hopperDutyCycle;	// Motor duty cycle percent power, -1 to 1
-  private final StatusSignal<Current> hopperStatorCurrent; // Motor stator current, in amps (+=fwd, -=rev)
-  private final StatusSignal<Angle> hopperEncoderPosition; // Encoder position, in pinion rotations
-  private final StatusSignal<AngularVelocity> hopperEncoderVelocity;	
+  private final TalonFX hopperMotor = new TalonFX(Constants.Ports.CANHopper);
+
+  // Create variables for the hopper Kraken motor
+  private final StatusSignal<Voltage> hopperSupplyVoltage;            // Incoming bus voltage to motor, in volts
+  private final StatusSignal<Temperature> hopperTemp;                 // Motor temperature, in degrees Celsius
+  private final StatusSignal<Double> hopperDutyCycle;                 // Motor duty cycle percent power, -1 to 1
+  private final StatusSignal<Current> hopperStatorCurrent;            // Motor stator current, in amps (positive = forward, negative = reverse)
+  private final StatusSignal<Angle> hopperEncoderPosition;            // Encoder position, in pinion rotations
+  private final StatusSignal<AngularVelocity> hopperEncoderVelocity;
   private final StatusSignal<Voltage> hopperVoltage;
 
-  // Create Kraken for hopper motor
-  private final TalonFX hopperMotor = new TalonFX(Constants.Ports.CANHopper);
   private final TalonFXConfigurator hopperConfigurator = hopperMotor.getConfigurator();
   private TalonFXConfiguration hopperConfig;
   private VoltageOut hopperVoltageControl = new VoltageOut(0.0); 
 
-  /**
-   * Creates the hopper subsystem
-   * @param subsystemName
-   * @param log
-   */
   public Hopper(String subsystemName, FileLog log) {
-    this.log = log; // save reference to the fileLog
     this.subsystemName = subsystemName;
+    this.log = log;
     logRotationKey = log.allocateLogRotation();
 
     // Get signal and sensor objects
@@ -64,18 +57,19 @@ public class Hopper extends SubsystemBase {
     hopperEncoderVelocity = hopperMotor.getVelocity();
     hopperVoltage = hopperMotor.getMotorVoltage();
 
-    // Configure the hopper motor
-    hopperConfig = new TalonFXConfiguration(); // Factory default configuration
+    // Configure the motor
+    hopperConfig = new TalonFXConfiguration();
     hopperConfig.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
     hopperConfig.MotorOutput.NeutralMode = NeutralModeValue.Brake;
     hopperConfig.Voltage.PeakForwardVoltage = HopperConstants.compensationVoltage;
     hopperConfig.Voltage.PeakReverseVoltage = -HopperConstants.compensationVoltage;
-    hopperConfig.OpenLoopRamps.VoltageOpenLoopRampPeriod = 0.3; // # seconds from 0 to full power
+    hopperConfig.OpenLoopRamps.VoltageOpenLoopRampPeriod = 0.3; // Time from 0 to full power, in seconds
 
-    // Supply current limit is typically used to prevent breakers from tripping
-    hopperConfig.CurrentLimits.SupplyCurrentLimit = 60.0; // (amps) If current is above this value for longer than threshold time, then limit current to the lower limit
-    hopperConfig.CurrentLimits.SupplyCurrentLowerLimit = 35.0; // (amps) Lower limit for the current
-    hopperConfig.CurrentLimits.SupplyCurrentLowerTime = 0.2; // (sec) Threshold time
+    // If the current is above the supply current limit for the threshold time, the current is limited to the lower limit.
+    // This is configured to prevent the breakers from tripping.
+    hopperConfig.CurrentLimits.SupplyCurrentLimit = 60.0;       // Upper limit for the current, in amps
+    hopperConfig.CurrentLimits.SupplyCurrentLowerLimit = 35.0;  // Lower limit for the current, in amps
+    hopperConfig.CurrentLimits.SupplyCurrentLowerTime = 0.2;    // Threshold time, in seconds
     hopperConfig.CurrentLimits.SupplyCurrentLimitEnable = true;
 
     // Set hopper motor configuration
@@ -83,30 +77,25 @@ public class Hopper extends SubsystemBase {
 
     stopHopperMotor();
   }
-  
 
   /**
-   * Returns the name of the subsystem
+   * Sets the percent of the hopper motor using voltage compensation.
+   * @param percent output percent, -1.0 to 1.0 (positive = intake, negative = reverse)
    */
-  public String getName() {
-    return subsystemName;
-  }
-
-  /** 
-   * Sets the percent of the hopper motor using voltage compensation
-   * @param percent percent 0.0 to 1.0 (+ = outtake coral)
-  */
   public void setHopperPercentOutput(double percent) {
-    hopperMotor.setControl(hopperVoltageControl.withOutput(percent*HopperConstants.compensationVoltage));
+    hopperMotor.setControl(hopperVoltageControl.withOutput(percent * HopperConstants.compensationVoltage));
   }
 
+  /**
+   * Stops the hopper motor.
+   */
   public void stopHopperMotor() {
     setHopperPercentOutput(0);
   }
 
   /**
-   * Get the velocity of the hopper motor
-   * @return hopper motor velocity, in RPM
+   * Gets the velocity of the hopper motor.
+   * @return motor velocity, in RPM
    */
   public double getHopperVelocity() {
     hopperEncoderVelocity.refresh();
@@ -114,34 +103,37 @@ public class Hopper extends SubsystemBase {
   }
 
   /**
-   * Get the current of the hopper motor
-   * @return hopper motor current, in amps
+   * Gets the current of the hopper motor.
+   * @return motor current, in amps
    */
   public double getHopperAmps() {
     hopperStatorCurrent.refresh();
     return hopperStatorCurrent.getValueAsDouble();
   }
 
-
-  @Override
-  public void periodic() {
-    if(fastLogging || log.isMyLogRotation(logRotationKey)) {
-      updateLog(false);
-    }
-  }
-
-  public void enableFastLogging(boolean enabled) {
-    fastLogging = enabled;
+  /**
+   * Gets the name of the subsystem.
+   * @return the subsystem name
+   */
+  public String getName() {
+    return subsystemName;
   }
 
   /**
-   * Write information about hopper to fileLog.
-   * @param logWhenDisabled true = log when disabled, false = discard the string
+   * Write information about hopper to the file log.
+   * @param logWhenDisabled true = write when robot is disabled, false = only write when robot is enabled
    */
   public void updateLog(boolean logWhenDisabled) {
     log.writeLog(logWhenDisabled, subsystemName, "Update Variables",
-      "hopper Current (Amps)", getHopperAmps(),
-      "hopper Velocity (RPM)", getHopperVelocity()
+      "Hopper Current (Amps)", getHopperAmps(),
+      "Hopper Velocity (RPM)", getHopperVelocity()
     );
+  }
+
+  @Override
+  public void periodic() {
+    if (fastLogging || log.isMyLogRotation(logRotationKey)) {
+      updateLog(false);
+    }
   }
 }
