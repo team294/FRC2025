54db47ea95531e5f4694abfa73cd8477ac06516d
Carolina <cabs10001@gmail.com>
2025-03-23 16:22:07 -0700

    Driving to the closest side of the barge
    Not driving under the barge when defending, but instead aligning to the scoring position on the side of the barge opposite the drivers.

--- src/main/java/frc/robot/utilities/Field.java
+++ src/main/java/frc/robot/utilities/Field.java
@@ -345,8 +345,10 @@ public class Field {
      * @return Pose2d of the nearest point we can score on the barge
      */
     public Pose2d getNearestBargeScoringPosition(Pose2d currPos){
-        Pose2d bargeScoringPos = (allianceSelection.getAlliance() == Alliance.Blue) ? aprilTagFieldLayout.getTagPose(14).get().toPose2d().transformBy(new Transform2d((FieldConstants.bargeScoringOffset), 0, new Rotation2d(0))) : aprilTagFieldLayout.getTagPose(5).get().toPose2d().transformBy(new Transform2d((FieldConstants.bargeScoringOffset), 0, new Rotation2d(0)));
-        bargeScoringPos = bargeScoringPos.rotateAround(bargeScoringPos.getTranslation(), new Rotation2d(Math.PI));
+        Pose2d bargeScoringPos = (allianceSelection.getAlliance() == Alliance.Blue) ? 
+          (currPos.getX() >  FieldConstants.length / 2.0) ? aprilTagFieldLayout.getTagPose(4).get().toPose2d() : aprilTagFieldLayout.getTagPose(14).get().toPose2d()
+        : (currPos.getX() >= FieldConstants.length / 2.0) ? aprilTagFieldLayout.getTagPose(5).get().toPose2d() : aprilTagFieldLayout.getTagPose(15).get().toPose2d();
+        bargeScoringPos = bargeScoringPos.rotateAround(bargeScoringPos.getTranslation(), new Rotation2d(Math.PI)).transformBy(new Transform2d((-FieldConstants.bargeScoringOffset), 0, new Rotation2d(0)));
 
         return new Pose2d(bargeScoringPos.getX(), MathUtil.clamp(currPos.getY(), bargeScoringPos.getY() - (FieldConstants.bargeScorableWidth / 2.0), bargeScoringPos.getY() + (FieldConstants.bargeScorableWidth / 2.0)), bargeScoringPos.getRotation());
     }
