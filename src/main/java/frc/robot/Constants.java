05916d8046880582fd5215b8355005c68301806a
Don Sawdai <dsawdai@ieee.org>
2025-03-28 21:39:15 -0700

    Calibrated Drivetrate ks, kv, ka for Phoenix Pro FOC

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -152,7 +152,7 @@ public final class Constants {
     public static final double kNominalAngularAccelerationRadiansPerSecondSquared = Math.PI;
 
     // CALIBRATED
-    public static final double kVDriveAvg = 2.251;  // In voltage per meters/second
+    public static final double kVDriveAvg = 2.392;  // In voltage per meters/second    LAR was 2.251 (FOC off), AVR is 2.392 (FOC on)
     private static final double kVmFLrel = 1.0;     // kV modifier for FL drive motor
     private static final double kVmFRrel = 1.0;     // kV modifier for FR drive motor
     private static final double kVmBLrel = 1.0;     // kV modifier for BL drive motor
@@ -166,9 +166,9 @@ public final class Constants {
     public static final double kVmBR = kVmBRrel / kVmAvg;
 
     public static final double dt = 0.02;             // CALIBRATED Timestep for discretizing robot motion, in seconds (scheduler time period = 20ms)
-    public static final double kADrive = 0.2692;      // CALIBRATED In voltage per meters per second^2
+    public static final double kADrive = 0.2383;      // CALIBRATED In voltage per meters per second^2   LAR was 0.2692 (FOC off), AVR is 0.2383 (FOC on)
     public static final double kADriveToPose = 0.050; // CALIBRATED on ETU in 3/2/2025 In seconds (On the ETU, DriveToPose behaves better with a small value for kADriveToPose)
-    public static final double kSDrive = 0.030;       // CALIBRATED In voltage
+    public static final double kSDrive = 0.141;       // CALIBRATED In voltage      LAR was 0.030 (FOC off), AVR is 0.141 (FOC on)
 
     // Minimum abs delta (in m/sec) between actual wheel velocity and desired wheel velocity for kADrive to be applied.
     // If the delta is less than this, then don't use kADrive. This prevents the drive motors from jittering.
