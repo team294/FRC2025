78cd9dc2d9f2aefd0ea09671ad66a2b32a0de918
Don Sawdai <dsawdai@ieee.org>
2025-04-04 14:38:56 -0700

    Reduced auto delay at HP station

--- src/main/java/frc/robot/commands/autos/components/AutoCoralDriveAndIntakeSequence.java
+++ src/main/java/frc/robot/commands/autos/components/AutoCoralDriveAndIntakeSequence.java
@@ -29,7 +29,7 @@ public class AutoCoralDriveAndIntakeSequence extends SequentialCommandGroup {
     addCommands(
       new DataLogMessage(false, "AutoCoralDriveAndIntakeSequence: Start, starting reef location =", start.toString()),
       new AutoDriveToHPAndPrep(start, driveTrain, elevator, wrist, hopper, coralEffector, alliance),
-      new CoralIntakeSequence(elevator, wrist, hopper, coralEffector).withTimeout(0.4),
+      new CoralIntakeSequence(elevator, wrist, hopper, coralEffector).withTimeout(0.294),
       new DataLogMessage(false, "AutoCoralDriveAndIntakeSequence: End")
     );
   }
