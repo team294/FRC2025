1c7c49541a80b3630638037772774b1680e3ed2b
Cameron Murray <cameron.s.murray57@gmail.com>
2025-04-09 19:13:32 -0700

    automated algae pickup sequence moved to work in conjuction with joystick button

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -249,8 +249,8 @@ public class RobotContainer {
         () -> !coralEffector.isCoralPresent()
       )
     );
-    xbPOVDown.onTrue(new AutomatedDriveToReefAndIntakeAlgae(ElevatorWristPosition.ALGAE_LOWER, driveTrain, elevator, wrist, algaeGrabber, led, field));
-    xbPOVLeft.onTrue(new AutomatedDriveToReefAndIntakeAlgae(ElevatorWristPosition.ALGAE_UPPER, driveTrain, elevator, wrist, algaeGrabber, led, field));
+    //xbPOVDown.onTrue(new AlgaeIntakeSequence(ElevatorWristPosition.ALGAE_LOWER, driveTrain, elevator, wrist, algaeGrabber, led));  //Replaced with AutomatedDriveToReefAndIntakeAlgae in conjuction with rigth joystick button
+    //xbPOVLeft.onTrue(new AlgaeIntakeSequence(ElevatorWristPosition.ALGAE_UPPER, driveTrain, elevator, wrist, algaeGrabber, led));
 
     // Prep and intake algae from Lollipop with Back
     xbBack.onTrue(either(
@@ -303,6 +303,9 @@ public class RobotContainer {
     right[1].and(xboxController.a()).whileTrue(new AutomatedDriveToReefAndScoreCoral(ReefLevel.L2, driveTrain, elevator, wrist, coralEffector, algaeGrabber, led, rightJoystick, field));
     right[1].and(xboxController.b()).whileTrue(new AutomatedDriveToReefAndScoreCoral(ReefLevel.L3, driveTrain, elevator, wrist, coralEffector, algaeGrabber, led, rightJoystick, field));
     right[1].and(xboxController.y()).whileTrue(new AutomatedDriveToReefAndScoreCoral(ReefLevel.L4, driveTrain, elevator, wrist, coralEffector, algaeGrabber, led, rightJoystick, field));
+
+    right[1].and(xboxController.povLeft()).whileTrue(new AutomatedDriveToReefAndIntakeAlgae(ElevatorWristPosition.ALGAE_UPPER, driveTrain, elevator, wrist, algaeGrabber, led, field))
+    right[1].and(xboxController.povDown()).whileTrue(new AutomatedDriveToReefAndIntakeAlgae(ElevatorWristPosition.ALGAE_LOWER, driveTrain, elevator, wrist, algaeGrabber, led, field))
 
     right[2].whileTrue(new DriveToBargeWithOdometry(driveTrain, field));
   }
